5af5759839058c4cf27d5cef52f60340802671cb
Shadab Alam <shadabalam1419@gmail.com>
2026-08-03 16:40:16 +0530

    M02/T01/ST01/work

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,5 +2,6 @@ public class WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Welcome to Java track");
         System.out.println("I already know how to build programming logic");
+
     }
 }
