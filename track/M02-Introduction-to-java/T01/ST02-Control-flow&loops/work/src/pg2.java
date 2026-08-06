a13246780f645c8dfeac7467931c3e25b1ad7862
Shadab Alam <shadabalam1419@gmail.com>
2026-08-06 15:35:22 +0530

    while loop

---
+++ track/M02-Introduction-to-java/T01/ST02-Control-flow&loops/work/src/pg2.java
@@ -0,0 +1,13 @@
+public class pg2 {
+    public static  void main(String[] args){
+        // while loop
+        // use while loop when we know the staring point and ending point
+
+        int i = 1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
