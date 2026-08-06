2663f78652af02d3d029e9bd1450ee3d968695ab
Shadab Alam <shadabalam1419@gmail.com>
2026-08-06 15:37:45 +0530

    do while loop

--- track/M02-Introduction-to-java/T01/ST02-Control-flow&loops/work/src/pg2.java
+++ track/M02-Introduction-to-java/T01/ST02-Control-flow&loops/work/src/pg2.java
@@ -1,8 +1,7 @@
 public class pg2 {
     public static  void main(String[] args){
         // while loop
-        // use while loop when we know the staring point and ending point
-
+        // Use a while loop when the number of iterations is not known in advance. The loop continues as long as a condition is true.
         int i = 1;
         while(i<=5){
             System.out.println(i);
---
+++ track/M02-Introduction-to-java/T01/ST02-Control-flow&loops/work/src/pg3.java
@@ -0,0 +1,13 @@
+public class pg3 {
+    public static void main (String[] args){
+        // do while loop
+        // The do-while loop is similar to the while loop, but it executes the loop body at least once before checking the condition.
+        
+        int i = 1;
+        do{
+            System.out.println("Hello ! From  do while loop " + i);
+            i++;
+        }while(i<=5);
+
+    }
+}
