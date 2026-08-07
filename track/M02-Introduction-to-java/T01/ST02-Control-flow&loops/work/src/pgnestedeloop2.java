fe86ef513460c39fb965edf58dce2c6db55b6f15
Shadab Alam <shadabalam1419@gmail.com>
2026-08-07 14:51:25 +0530

    nested loop with while loop

---
+++ track/M02-Introduction-to-java/T01/ST02-Control-flow&loops/work/src/pgnestedeloop2.java
@@ -0,0 +1,24 @@
+public class pgnestedeloop2 {
+
+    public static void main(String[] args) {
+        
+        int i =5;
+
+        while (i <=10) {
+
+            int j= 5;
+            
+            while (j <=10){
+
+                System.out.println(j);
+
+                j++;
+            }
+
+            System.out.println("");
+            i++;
+        }
+
+    }
+
+}
